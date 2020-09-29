d6db192d13985d05d91d8c6935362974d03ef043
kevin_wang@sjtu.edu.cn <kevin_wang@sjtu.edu.cn>
2020-09-29 17:57:01 +0800

    添加router

--- core/router.js
+++ core/router.js
@@ -4,4 +4,7 @@ module.exports = (app) => {
     // 在这里写所有注册函数
     let dispatch = require("../src/dispatch.js");
     register(app, "post", "/dispatch", dispatch.post);
+
+    let routine = require("../src/routine");
+    register(app, "post", "/dispatch/routine", routine.get);
 };
